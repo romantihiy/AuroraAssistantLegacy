7d5f3f2e848d7b994cea9c2b53c6a28c829dcd40
Yury <75903645+sokil26@users.noreply.github.com>
2021-09-28 11:12:07 +0300

    Add files via upload

--- Parsing_Dataset.py
+++ Parsing_Dataset.py
@@ -5,6 +5,73 @@ import re
 
 answers = pd.read_csv('posts_answers.csv')
 questions = pd.read_csv('posts_questions.csv')
+cdata = questions[['id', 'title', 'body', 'tags']]
+cdata['tags'] = data['tags'].astype('str')
+cdata.head()
+mltags = '''
+tensorflow
+
+tensorflow2.0
+
+tensorflow-gpu
+
+tensorflow2.x
+
+tensorflow1.15
+
+tensorflow2
+
+tensorflow-layers
+
+tensorflow1.12
+
+torch
+
+pytorch
+
+torchvision
+
+torchtext
+
+pytorch-dataloader
+
+gpytorch
+
+torchaudio
+
+pytorch3d
+
+keras
+
+keras-2
+
+keras-metrics
+
+keras-rl
+
+keras-vggface
+
+keras-lambda
+
+tf.keras
+
+keras-layer
+
+datascience
+
+data science
+
+ml'''
+mltags = mltags.split('\n')
+mltags = [tag for tag in mltags if tag != '']
+mltags, len(mltags)
+mask = pd.Series(-1, np.arange(len(cdata)))
+for tag in mltags:
+    mask = mask + cdata['tags'].str.find(tag) + 1
+mask[mask >= 0] = 1
+mask[mask < 0] = 0
+mask = mask.astype('bool')
+mldata = cdata[mask]
 questions.drop(columns=["parent_id"], inplace=True)
 questions.rename(columns={"id" : "parent_id", "body" : "questions"}, inplace=True)
 answers.rename(columns={"body" : "answers"}, inplace=True)
